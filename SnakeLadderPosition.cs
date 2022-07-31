19d9f0dc4a87063c568337c792cc9832e6d1bfe9
bhumika1111 <bhoyarpatil@434>
2022-07-31 15:22:52 +0530

    Check Starting Position Of Player

---
+++ SnakeLadderPosition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    /// <summary>
+    /// Here We Check The POsition Of Player He Starts The Game For Position 0.
+    /// </summary>
+    public class SnakeLadderPosition
+    {
+        /// <summary>
+        /// Checks the player position.
+        /// </summary>
+        public static void CheckPlayerPosition()
+        {
+            int position = 0;
+            Console.WriteLine("Player position is:" + position);
+        }
+    }
+}
