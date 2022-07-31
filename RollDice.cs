631c32bf0c74cd849684c4c687f656fee270d305
bhumika1111 <bhoyarpatil@434>
2022-07-31 15:26:49 +0530

    Check The Roll Dice Number

---
+++ RollDice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    /// <summary>
+    /// Here We Check The Player Roll Die By Using Random Generator.
+    /// </summary>
+    public class RollDice
+    {
+        /// <summary>
+        /// Checks the player rolls die.
+        /// </summary>
+        public static void CheckPlayerRollsDie()
+        {
+            int position = 0;
+            Console.WriteLine("Player Starts From Position 0:", position);
+            Random random = new Random();
+            int dieRoll = random.Next(1, 7);
+            position = dieRoll;
+            Console.WriteLine("position:" + position);
+
+
+            
+
+        }
+    }
+}
